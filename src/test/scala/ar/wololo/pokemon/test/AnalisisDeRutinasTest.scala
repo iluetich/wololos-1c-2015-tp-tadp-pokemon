0673527e8a844eac5a284310b40c084e90e3947c
Ivan <ivan.luetich93@gmail.com>
2015-06-08 12:37:37 -0300

    Actualizo tests integrando con el branch 'actividades'

--- src/test/scala/ar/wololo/pokemon/test/AnalisisDeRutinasTest.scala
+++ src/test/scala/ar/wololo/pokemon/test/AnalisisDeRutinasTest.scala
@@ -17,33 +17,30 @@ import ar.wololo.pokemon.dominio.SuperSistemaDeAnalisis.NoHuboRutinaHacibleExcep
 import ar.wololo.pokemon.dominio.MaxEnergia
 import ar.wololo.pokemon.dominio.Bueno
 import ar.wololo.pokemon.dominio.SuperSistemaDeAnalisis.NoHuboRutinaHacibleException
+import ar.wololo.pokemon.dominio.UsarPocion
 
 class AnalisisDeRutinasTest extends FunSuite {
 
   test("Si a un pokemon sólo lo hago hacer una rutina, esa es la mejor, salvo que no pueda hacerla") {
-    /*
-     * TODO Este test fallará hasta que se implemente la actividad Nadar
-     */
     var pikachu = new Pokemon(Bueno, null, Electrico, null, 1, 100, Macho, 100, 100, 1000, 100, 100, SubirDeNivel)
-    var rutinaNado = new Rutina(List[Actividad](Nadar, Nadar, Nadar, Nadar))
+    var rutinaPocionado = new Rutina(List[Actividad](UsarPocion, UsarPocion, UsarPocion, UsarPocion))
     var rutinaIntercambio = new Rutina(List[Actividad](FingirIntercambio, FingirIntercambio, FingirIntercambio))
-    var mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaNado), MinPeso)
+    var mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaPocionado), MinPeso)
 
-    assert(mejorRutina.get === rutinaNado)
+    assert(mejorRutina.get === rutinaPocionado)
+    
     pikachu = pikachu.copy(estado = Ko)
-    intercept[NoHuboRutinaHacibleException](SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaNado), MinPeso))
+    
+    intercept[NoHuboRutinaHacibleException](SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaPocionado), MinPeso))
   }
 
-  test("Si pikachu es macho y su condicion evolutiva es SubirDeNivel y debe elegir entre nadar y fingir el intercambio, es mejor nadar para bajar de peso") {
-    /*
-     * TODO Este test fallará hasta que se implemente la actividad Nadar
-     */
-    var pikachu = new Pokemon(Bueno, null, Electrico, null, 1, 100, Macho, 100, 100, 1000, 100, 100, SubirDeNivel)
-    var rutinaNado = new Rutina(List[Actividad](Nadar, Nadar, Nadar, Nadar))
+  test("Si pikachu es macho y su condicion evolutiva es SubirDeNivel y debe elegir entre rutinas de UsarPocion y FingirIntercambio, es mejor UsarPocion para aumentar energía") {
+    var pikachu = new Pokemon(Bueno, null, Electrico, null, 1, 100, Macho, 50, 300, 1000, 100, 100, SubirDeNivel)
+    var rutinaPocionado = new Rutina(List[Actividad](UsarPocion, UsarPocion, UsarPocion, UsarPocion))
     var rutinaIntercambio = new Rutina(List[Actividad](FingirIntercambio, FingirIntercambio, FingirIntercambio))
-    var mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaNado, rutinaIntercambio), MinPeso)
+    var mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaIntercambio, rutinaPocionado), MaxEnergia)
 
-    assert(mejorRutina.get === rutinaNado)
+    assert(mejorRutina.get === rutinaPocionado)
   }
   
   test("Si no paso rutinas sobre las cuales analizar la mejor, tira excepción") {
@@ -56,8 +53,9 @@ class AnalisisDeRutinasTest extends FunSuite {
     var pikachu = new Pokemon(Ko, null, Electrico, null, 1, 100, Macho, 100, 100, 1000, 100, 100, SubirDeNivel)
     var rutinaNado = new Rutina(List[Actividad](Nadar, Nadar, Nadar, Nadar))
     var rutinaIntercambio = new Rutina(List[Actividad](FingirIntercambio, FingirIntercambio, FingirIntercambio))
-
-    intercept[NoHuboRutinaHacibleException](SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaNado, rutinaIntercambio), MinPeso))
+    var rutinaPocionado = new Rutina(List[Actividad](UsarPocion, UsarPocion, UsarPocion, UsarPocion))
+    
+    intercept[NoHuboRutinaHacibleException](SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pikachu, List[Rutina](rutinaNado, rutinaIntercambio, rutinaPocionado), MinPeso))
   }
 
 }
