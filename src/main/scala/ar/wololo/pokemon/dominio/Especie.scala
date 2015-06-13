b7866d4fd48ba42b11e255e6d621f205afe85598
Ivan <ivan.luetich93@gmail.com>
2015-06-13 16:34:32 -0300

    Agrego validación sobre peso máximo saludable y mejoro delegación en Pokemón.

--- src/main/scala/ar/wololo/pokemon/dominio/Especie.scala
+++ src/main/scala/ar/wololo/pokemon/dominio/Especie.scala
@@ -1,36 +1,36 @@
 package ar.wololo.pokemon.dominio
 
 abstract class Especie(val tipoPrincipal: Tipo,
-  val tipoSecundario: Tipo,
+  val tipoSecundario: Tipo = null,
   val incrementoFuerza: Int,
   val incrementoVelocidad: Int,
   val incrementoPeso: Int,
   val incrementoEnergiaMax: Int,
+  val pesoMaximoSaludable: Int,
   val resistenciaEvolutiva: Int,
   val condicionEvolutiva: CondicionEvolutiva,
   val especieEvolucion: Especie = null) {
 
+  def experienciaParaNivel(nivel: Int): Integer = {
+			nivel match {
+			case 0 => resistenciaEvolutiva
+			case n => 2 * experienciaParaNivel(n - 1) + resistenciaEvolutiva
+			}
+	}
+	
+	def evolucionarA(pokemon: Pokemon): Pokemon = {
+			pokemon.copy(especie = especieEvolucion)
+	}
+	
   def subirDeNivelA(pokemon: Pokemon): Pokemon = {
     val nivelNuevo = pokemon.nivel + 1
     val fuerzaNueva = pokemon.fuerza + incrementoFuerza
-    val pesoNuevo = pokemon.peso + incrementoPeso
     val energiaMaxNueva = pokemon.energiaMax + incrementoEnergiaMax
-    val pokemonMejorado = pokemon.copy(nivel = nivelNuevo, fuerza = fuerzaNueva, peso = pesoNuevo, energiaMax = energiaMaxNueva)
+    val pokemonMejorado = pokemon.copy(nivel = nivelNuevo, fuerza = fuerzaNueva, energiaMax = energiaMaxNueva).aumentaPeso(incrementoPeso)
 
     condicionEvolutiva match {
       case c: SubirDeNivel if (c.nivelParaEvolucionar == nivelNuevo) => evolucionarA(pokemonMejorado)
       case _ => pokemonMejorado
-    }
-  }
-
-  def evolucionarA(pokemon: Pokemon): Pokemon = {
-    pokemon.copy(especie = especieEvolucion)
-  }
-
-  protected def experienciaParaNivel(nivel: Int): Integer = {
-    nivel match {
-      case 0 => resistenciaEvolutiva
-      case n => 2 * experienciaParaNivel(n - 1) + resistenciaEvolutiva
     }
   }
 
@@ -48,5 +48,20 @@ abstract class Especie(val tipoPrincipal: Tipo,
           pokemon.copy(experiencia = pokemon.experiencia + expAcum)
         }
     }
+  }
+
+  /*
+   * [..] También cada especie determina un Peso Máximo, que indica qué tanto Peso
+   * pueden ganar sus miembros antes de que afecte su salud [...] -> Página 3 del TP
+   */
+  
+  def aumentaPesoDe(pokemon: Pokemon, cantidad: Integer): Pokemon = {
+    val pesoFuturo = pokemon.peso + cantidad
+    if (pesoFuturo > pesoMaximoSaludable)
+      //Afecta su salud, pero el requerimiento no especifica cómo.
+      //Duplico código porque no sé si hay que hacer algo distinto o no.
+      pokemon.copy(peso = pesoFuturo)
+    else
+      pokemon.copy(peso = pesoFuturo)
   }
 }
