12cc79571e7136da0df77a0daea21cec5a10e75b
Ivan <ivan.luetich93@gmail.com>
2015-06-15 00:13:34 -0300

    Renombro método más expresivo.

--- src/main/scala/ar/wololo/pokemon/dominio/Tipos.scala
+++ src/main/scala/ar/wololo/pokemon/dominio/Tipos.scala
@@ -1,18 +1,18 @@
 package ar.wololo.pokemon.dominio
 
-trait Tipo { def leGanaA(): List[Tipo] }
-object Fuego extends Tipo { def leGanaA(): List[Tipo] = List(Planta, Hielo, Bicho) }
-object Agua extends Tipo { def leGanaA(): List[Tipo] = List(Fuego, Tierra, Roca) }
-object Planta extends Tipo { def leGanaA(): List[Tipo] = List(Agua, Tierra, Roca) }
-object Tierra extends Tipo { def leGanaA(): List[Tipo] = List(Fuego, Electrico, Veneno, Roca) }
-object Hielo extends Tipo { def leGanaA(): List[Tipo] = List(Planta, Tierra, Volador, Dragon) }
-object Roca extends Tipo { def leGanaA(): List[Tipo] = List(Fuego, Hielo, Volador, Bicho) }
-object Electrico extends Tipo { def leGanaA(): List[Tipo] = List(Agua, Volador) }
-object Psiquico extends Tipo { def leGanaA(): List[Tipo] = List(Pelea, Veneno) }
-object Pelea extends Tipo { def leGanaA(): List[Tipo] = List(Normal, Hielo, Roca) }
-object Fantasma extends Tipo { def leGanaA(): List[Tipo] = List(Psiquico, Fantasma) }
-object Volador extends Tipo { def leGanaA(): List[Tipo] = List(Planta, Pelea, Bicho) }
-object Bicho extends Tipo { def leGanaA(): List[Tipo] = List(Planta, Psiquico) }
-object Veneno extends Tipo { def leGanaA(): List[Tipo] = List(Planta) }
-object Dragon extends Tipo { def leGanaA(): List[Tipo] = List(Dragon) }
-object Normal extends Tipo { def leGanaA(): List[Tipo] = List() }
+trait Tipo { def aQuienesLeGanas(): List[Tipo] }
+object Fuego extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Planta, Hielo, Bicho) }
+object Agua extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Fuego, Tierra, Roca) }
+object Planta extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Agua, Tierra, Roca) }
+object Tierra extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Fuego, Electrico, Veneno, Roca) }
+object Hielo extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Planta, Tierra, Volador, Dragon) }
+object Roca extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Fuego, Hielo, Volador, Bicho) }
+object Electrico extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Agua, Volador) }
+object Psiquico extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Pelea, Veneno) }
+object Pelea extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Normal, Hielo, Roca) }
+object Fantasma extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Psiquico, Fantasma) }
+object Volador extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Planta, Pelea, Bicho) }
+object Bicho extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Planta, Psiquico) }
+object Veneno extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Planta) }
+object Dragon extends Tipo { def aQuienesLeGanas: List[Tipo] = List(Dragon) }
+object Normal extends Tipo { def aQuienesLeGanas: List[Tipo] = List() }
