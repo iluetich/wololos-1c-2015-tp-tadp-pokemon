c15d6255206f66d5a66e14f839e99cb4cb544728
Ivan <ivan.luetich93@gmail.com>
2015-06-18 19:19:24 -0300

    Ahora el análisis retorna un Option[String].

--- src/main/scala/ar/wololo/pokemon/dominio/CriterioRutina.scala
+++ src/main/scala/ar/wololo/pokemon/dominio/CriterioRutina.scala
@@ -11,14 +11,14 @@ import scala.util.Try
  */
 
 abstract class CriterioRutina {
-  def obtenerMejorRutina(rutinasYPokemones: Seq[(String, Try[Pokemon])]): String
+  def obtenerMejorRutina(rutinasYPokemones: Seq[(Rutina, Try[Pokemon])]): Option[Rutina]
 }
 case object MaxNivel extends CriterioRutina {
-  def obtenerMejorRutina(rutinasYPokemones: Seq[(String, Try[Pokemon])]): String = rutinasYPokemones.maxBy { _._2.get.nivel }._1
+  def obtenerMejorRutina(rutinasYPokemones: Seq[(Rutina, Try[Pokemon])]): Option[Rutina] = Some(rutinasYPokemones.maxBy { _._2.get.nivel }._1)
 }
 case object MaxEnergia extends CriterioRutina {
-  def obtenerMejorRutina(rutinasYPokemones: Seq[(String, Try[Pokemon])]): String = rutinasYPokemones.maxBy { _._2.get.energia }._1
+  def obtenerMejorRutina(rutinasYPokemones: Seq[(Rutina, Try[Pokemon])]): Option[Rutina] = Some(rutinasYPokemones.maxBy { _._2.get.energia }._1)
 }
 case object MinPeso extends CriterioRutina {
-  def obtenerMejorRutina(rutinasYPokemones: Seq[(String, Try[Pokemon])]): String = rutinasYPokemones.minBy { _._2.get.peso }._1
+  def obtenerMejorRutina(rutinasYPokemones: Seq[(Rutina, Try[Pokemon])]): Option[Rutina] = Some(rutinasYPokemones.minBy { _._2.get.peso }._1)
 }
--- src/main/scala/ar/wololo/pokemon/dominio/SuperSistemaDeAnalisis.scala
+++ src/main/scala/ar/wololo/pokemon/dominio/SuperSistemaDeAnalisis.scala
@@ -11,12 +11,11 @@ object SuperSistemaDeAnalisis {
 
   case class NoHuboRutinaHacibleException(pokemon: Pokemon) extends Exception
 
-  def obtenerMejorRutinaSegun(pokemon: Pokemon, rutinas: Seq[Rutina], criterio: CriterioRutina): String = {
-    var rutinasYPokemones = rutinas.map { rutina => (rutina.nombre, pokemon.realizarRutina(rutina)) }.filter { case (_, e) => e.isSuccess }
-    
+  def obtenerMejorRutinaSegun(pokemon: Pokemon, rutinas: Seq[Rutina], criterio: CriterioRutina): Option[String] = {
+    val rutinasYPokemones = rutinas.map { rutina => (rutina, pokemon.realizarRutina(rutina)) }.filter { case (_, e) => e.isSuccess }
     rutinasYPokemones.size match {
-      case 0 => throw NoHuboRutinaHacibleException(pokemon)
-      case _ => criterio.obtenerMejorRutina(rutinasYPokemones)
+      case 0 => None
+      case _ => Some(criterio.obtenerMejorRutina(rutinasYPokemones).fold { "No hubo mejor rutina" } { rutina => rutina.nombre })
     }
   }
 }
