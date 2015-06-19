8ee6b5957e08418f2a07c93b59487f8ed7b2f38b
Ivan <ivan.luetich93@gmail.com>
2015-06-18 21:00:06 -0300

    Currifico el análisis de rutinas =)

--- src/main/scala/ar/wololo/pokemon/dominio/SuperSistemaDeAnalisis.scala
+++ src/main/scala/ar/wololo/pokemon/dominio/SuperSistemaDeAnalisis.scala
@@ -4,12 +4,28 @@ import scala.util.Try
 import scala.util.Success
 
 object SuperSistemaDeAnalisis {
-
-  def obtenerMejorRutinaSegun(pokemon: Pokemon, rutinas: Seq[Rutina], criterio: CriterioRutina): Option[String] = {
+/*
+ * Criterios como objetos
+ */
+  def obtenerMejorRutinaSegun(pokemon: Pokemon)(rutinas: Seq[Rutina])(criterio: CriterioRutina): Option[String] = {
     val rutinasYPokemones = rutinas.map { rutina => (rutina, pokemon.realizarRutina(rutina)) }.filter { case (_, e) => e.isSuccess }
     rutinasYPokemones.size match {
       case 0 => None
       case _ => Some(criterio.obtenerMejorRutina(rutinasYPokemones).fold { "No hubo mejor rutina" } { rutina => rutina.nombre })
     }
   }
+/*
+ * Criterios como funciones
+ */
+//  def obtenerMejorRutinaSegun(pokemon: Pokemon) (rutinas: Seq[Rutina], mejorRutina: Seq[(Rutina, Pokemon)] => Rutina): Option[String] = {
+//    val rutinasYPokemones = aplicarA(pokemon, rutinas)
+//    rutinasYPokemones.size match {
+//      case 0 => None
+//      case _ => Some(mejorRutina(rutinasYPokemones).nombre)
+//    }
+//  }
+//
+//  private def aplicarA(pokemon: Pokemon, rutinas: Seq[Rutina]): Seq[(Rutina, Pokemon)] = rutinas.map { rutina => (rutina, pokemon.realizarRutina(rutina)) }
+//    .filter { case (_, e) => e.isSuccess }
+//    .map { case (r, e) => (r, e.get) }
 }
--- src/test/scala/ar/wololo/pokemon/test/AnalisisDeRutinasTest.scala
+++ src/test/scala/ar/wololo/pokemon/test/AnalisisDeRutinasTest.scala
@@ -4,52 +4,54 @@ import org.scalatest.FunSuite
 import ar.wololo.pokemon.dominio._
 
 class AnalisisDeRutinasTest extends FunSuite {
-  
+
   def fixture = Fixt
+  def obtenerMejorRutinaSegun(rutinas: Seq[Rutina])(criterio: CriterioRutina) = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu)(rutinas)(criterio)
 
   test("Si a un pokemon sólo lo hago hacer una rutina, esa es la mejor, salvo que no pueda hacerla") {
     val pocionado = fixture.rutinaPocionado
-    val mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List(pocionado), MaxEnergia)
+    val mejorRutina = obtenerMejorRutinaSegun(List(pocionado))(MaxEnergia)
     assert(mejorRutina === Some(pocionado.nombre))
-    print("JAJAJAJ")
+
     val pokeKo = fixture.pikachu.copy(estado = Ko)
-    assert(SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pokeKo, List[Rutina](fixture.rutinaPocionado), MaxEnergia) === None)
+    assert(obtenerMejorRutinaSegun(List[Rutina](fixture.rutinaPocionado))(MaxEnergia) === None)
   }
 
   test("Si pikachu es macho y su condicion evolutiva es SubirDeNivel le conviene la rutina de pocionado para aumentar energía") {
     val pocionado = fixture.rutinaPocionado
     val intercambiado = fixture.rutinaIntercambio
-    val mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List(pocionado, intercambiado), MinPeso)
+    val mejorRutina = obtenerMejorRutinaSegun(List(pocionado, intercambiado))(MinPeso)
     assert(mejorRutina === Some(pocionado.nombre))
   }
-  
+
   test("Si no paso rutinas el análisis arroja excepción para cualquier criterio") {
-    assert(SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List[Rutina](), MaxEnergia) === None)
-    assert(SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List[Rutina](), MaxNivel) === None)
-    assert(SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List[Rutina](), MinPeso) === None)
+    def analisisAux(criterio: CriterioRutina) = obtenerMejorRutinaSegun(List[Rutina]())(criterio)
+    
+    assert(analisisAux(MaxEnergia) === None)
+    assert(analisisAux(MaxNivel) === None)
+    assert(analisisAux(MinPeso) === None)
   }
 
   test("Si pikachu está knockout el análisis no retorna ningún nombre") {
     val pokeKo = fixture.pikachu.copy(estado = Ko)
-    assert(SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(pokeKo, List[Rutina](fixture.rutinaNado, fixture.rutinaIntercambio, fixture.rutinaPocionado), MinPeso) === None)
+    assert(obtenerMejorRutinaSegun(List[Rutina](fixture.rutinaNado, fixture.rutinaIntercambio, fixture.rutinaPocionado))(MinPeso) === None)
   }
-  
+
   test("Conviene más nadar lo máximo posible para subir de nivel que usar pociones") {
     val pocionado = fixture.rutinaPocionado
     val nado = fixture.rutinaNado
-    val mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List(pocionado, nado), MaxNivel)
+    val mejorRutina = obtenerMejorRutinaSegun(List(pocionado, nado))(MaxNivel)
     assert(mejorRutina === Some(nado.nombre))
   }
-  
+
   test("Que el pokemon no pueda realizar una rutina no impide que realice otras") {
     val pocionado = fixture.rutinaPocionado
     val nado = fixture.rutinaNado
     val nadoQueNoPuedeHacer = fixture.rutinaPhelps
-    assert(SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List(nadoQueNoPuedeHacer), MaxNivel) === None)
-   
-    val mejorRutina = SuperSistemaDeAnalisis.obtenerMejorRutinaSegun(fixture.pikachu, List(nadoQueNoPuedeHacer, pocionado, nado), MaxNivel)
+    assert(obtenerMejorRutinaSegun(List(nadoQueNoPuedeHacer))(MaxNivel) === None)
+
+    val mejorRutina = obtenerMejorRutinaSegun(List(nadoQueNoPuedeHacer, pocionado, nado))(MaxNivel)
     assert(mejorRutina === Some(nado.nombre))
   }
-  
 
 }
