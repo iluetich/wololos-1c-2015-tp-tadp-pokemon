001e0bbf437ca545d3b0020db480a890833ba362
Ivan <ivan.luetich93@gmail.com>
2015-06-21 21:01:26 -0300

    Elimino duplicación de lógica.

--- src/main/scala/ar/wololo/pokemon/dominio/Genero.scala
+++ src/main/scala/ar/wololo/pokemon/dominio/Genero.scala
@@ -1,14 +1,16 @@
 package ar.wololo.pokemon.dominio
 
-abstract class Genero{
-  def fingiIntercambio(unPokemon: Pokemon):Pokemon
-  def aumentaExperiencia(unPokemon: Pokemon):Pokemon
+abstract class Genero {
+  def fingiIntercambio(unPokemon: Pokemon): Pokemon
+  def aumentaExperiencia(unPokemon: Pokemon): Pokemon
+  protected def fingiIntercambio(unPokemon: Pokemon, peso: Int) = unPokemon.modificaPeso(peso)
+  protected def aumentaExperiencia(unPokemon: Pokemon, exp: Int) = unPokemon.aumentaExperiencia(exp)
 }
-object Macho extends Genero{
-  def fingiIntercambio(unPokemon : Pokemon):Pokemon = unPokemon.modificaPeso(1)
-  def aumentaExperiencia(unPokemon : Pokemon):Pokemon = unPokemon.aumentaExperiencia(20)
+object Macho extends Genero {
+  def fingiIntercambio(poke: Pokemon) = fingiIntercambio(poke, 1)
+  def aumentaExperiencia(poke: Pokemon) = aumentaExperiencia(poke, 20)
 }
-object Hembra extends Genero{
-  def fingiIntercambio(unPokemon :Pokemon):Pokemon = unPokemon.modificaPeso(-10)
-  def aumentaExperiencia(unPokemon : Pokemon):Pokemon = unPokemon.aumentaExperiencia(40)
+object Hembra extends Genero {
+  def fingiIntercambio(poke: Pokemon) = fingiIntercambio(poke, -10)
+  def aumentaExperiencia(poke: Pokemon) = aumentaExperiencia(poke, 40)
 }
